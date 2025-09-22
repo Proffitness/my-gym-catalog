964bb274af4783535abaca6d98d4ba7bc49c9061
Proffitness <proffitness.hub@gmail.com>
2025-09-22 14:03:09 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,17 @@
+const gymData = [
+    {
+        number: "1",
+        name: "Жим ногами Technogym Leg Press Artis Unity Mini (з консоллю)",
+        photos: [
+            { url: "https://i.ibb.co/1JZsRjBG/1-1.jpg" },
+            { url: "https://i.ibb.co/WpMpWFJ6/1-2.jpg" }
+        ]
+    },
+    {
+        number: "2",
+        name: "Професійний вантажно-блоковий тренажер для Присідань Technogym Squat Artis Unity Mini (з консоллю)",
+        photos: [
+            { url: "https://i.ibb.co/CsP9V49p/2-1.jpg" }
+        ]
+    }
+];
