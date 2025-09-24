b771c77af6100c42ead92609ec8c77379add0208
Proffitness <proffitness.hub@gmail.com>
2025-09-24 12:06:52 +0300

    Update data.js

--- data.js
+++ data.js
@@ -16,6 +16,16 @@ const gymData = [
             { url: "./images/2/technogym-artis-squat-02.jpg" },
             { url: "./images/2/technogym-artis-squat-03.jpg" }
         ]
+    },
+    {
+        number: "2",
+        name: "Професійний вантажно-блоковий тренажер Жим від грудей Technogym Chest Press Artis Unity Mini (з консоллю)",
+        photos: [
+            { url: "./images/3/technogym-artis-chest-press-01.jpg" },
+            { url: "./images/3/technogym-artis-chest-press-02.jpg" },
+            { url: "./images/3/technogym-artis-chest-press-03.jpg" },
+            { url: "./images/3/technogym-artis-chest-press-03.jpg" },
+        ]
     }
 ]
 
