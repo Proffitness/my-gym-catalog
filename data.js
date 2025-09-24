9139721c8a1c0c4f6da4d0277c5c37dd37793c9e
Proffitness <proffitness.hub@gmail.com>
2025-09-24 11:47:25 +0300

    Update data.js

--- data.js
+++ data.js
@@ -3,18 +3,18 @@ const gymData = [
         number: "1",
         name: "Жим ногами Technogym Leg Press Artis Unity Mini (з консоллю)",
         photos: [
-            { url: "https://i.ibb.co/qMGMZJKg/Technogym-artis-leg-press-01.jpg" },
-            { url: "https://i.ibb.co/6cF7N16Z/Technogym-artis-leg-press-02.jpg" },
-            { url: "https://i.ibb.co/F44MVYSd/Technogym-artis-leg-press-03.jpg" }
+            { url: "./images/1/technogym-artis-leg-press-01.jpg" },
+            { url: "./images/1/technogym-artis-leg-press-02.jpg" },
+            { url: "./images/1/technogym-artis-leg-press-03.jpg" }
         ]
     },
     {
         number: "2",
         name: "Професійний вантажно-блоковий тренажер для Присідань Technogym Squat Artis Unity Mini (з консоллю)",
         photos: [
-            { url: "https://i.ibb.co/HLKSnK15/Technogym-artis-squat-01.jpg" },
-            { url: "https://i.ibb.co/XkxNWwr2/Technogym-artis-squat-02.jpg" },
-            { url: "https://i.ibb.co/1Jm2TcFJ/Technogym-artis-squat-03.jpg" }
+            { url: "./images/2/technogym-artis-squat-01.jpg" },
+            { url: "./images/2/technogym-artis-squat-02.jpg" },
+            { url: "./images/2/technogym-artis-squat-03.jpg" }
         ]
     }
 ]
